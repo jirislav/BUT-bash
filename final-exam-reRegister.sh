ee3a0832fd8108f097165a082bedb7292f511682
jirislav <mail@jkozlovsky.cz>
2015-12-02 13:38:48 +0100

    Moved common function to helper.sh

--- final-exam-reRegister.sh
+++ final-exam-reRegister.sh
@@ -1,112 +1,13 @@
 #!/bin/bash
 
-DIR="$(dirname $0)"
-
-configFileName="$DIR/config"
-
-# Load the config
-if [ -f "$configFileName" ]; then
-	source "$configFileName"
-else
-	echo "Config not found at '$configFileName'!"
-	exit 6
-fi
-
-cookieA="$DIR/.cookieA"
-cookieB="$DIR/.cookieB"
-html="$DIR/.html"
-
-switchCookies() {
-	tmp="$cookieA"
-	cookieA="$cookieB"
-	cookieB="$tmp"
-}
-
-login() {
-
-	# Get the required generated hidden intputs
-	URL="https://www.vutbr.cz/login/"
-	curl -Lc "$cookieA" "$URL" > "$html"
-	hiddenData=$(hxnormalize -edxL "$html" | hxselect -s '\n' "input[type=hidden]" | egrep -v "name=\"type|name=\"hact" |  awk 'BEGIN{ORS="&"; FS="\""}{print $2"="$6}')
-
-	if [ -z "$(echo "$hiddenData" | grep fdkey)" ]; then
-		echo "Empty FDKEY!"
-		exit 1
-	fi
-
-	# Check the login was in config
-	if [ -z "$login" ]; then
-		echo "Enter BUT login:"
-		read login
-
-		if [ -z "$login" ]; then
-			echo "Cannot use empty login!"
-			exit 2
-		fi
-	else
-		echo "Using BUT login from config '$login'"
-	fi
-
-	# Prompt for pw
-	echo "Enter password:" 
-	read -s pw
-
-	if [ -z "$pw" ]; then
-		echo "Cannot use empty password!"
-		exit 2
-	fi
-
-	data="${hiddenData}LDAPlogin=$login&LDAPpasswd=$pw"
-	URL="https://www.vutbr.cz/login/in"
-
-	justDoPOST
-}
-
-justDoPOST() {
-	tmpHtml="$html"
-	html="/dev/null"
-	getHtmlWithDataPOST
-	html="$tmpHtml"
-}
-
-assertURLnotEmpty() {
-
-	if [ -z "$URL" ]; then
-		echo "Cannot do request on unknown URL!"
-		exit 4
-	fi
-}
-
-assertDataNotEmpty() {
-
-	if [ -z "$data" ]; then
-		echo "Cannot do POST with no data!"
-		exit 3
-	fi
-}
-
-getHtmlWithDataPOST() {
-
-	assertDataNotEmpty
-	assertURLnotEmpty
-
-	switchCookies
-	curl -Lc "$cookieA" -b "$cookieB" -d "$data" "$URL" > "$html"
-}
-
-getHtml() {
-
-	assertURLnotEmpty
-
-	switchCookies
-	curl -Lc "$cookieA" -b "$cookieB" "$URL" > "$html"
-}
+helperFileName="$(dirname $0)/helper.sh"
+source $helperFileName
 
 login
 
 if [ -z "$1" ]; then
 	echo "No apid provided !"
-	exit 5
+	exit 2000
 fi
 
 # Set first argument to the apid of the subject to reRegister into
---
+++ helper.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+DIR="$(dirname $0)"
+
+configFileName="$DIR/config"
+
+# Load the config
+if [ -f "$configFileName" ]; then
+	source "$configFileName"
+else
+	echo "Config not found at '$configFileName'!"
+	exit 1000
+fi
+
+cookieA="$DIR/.cookieA"
+cookieB="$DIR/.cookieB"
+html="$DIR/.html"
+
+switchCookies() {
+	tmp="$cookieA"
+	cookieA="$cookieB"
+	cookieB="$tmp"
+}
+
+login() {
+
+	# Get the required generated hidden intputs
+	URL="https://www.vutbr.cz/login/"
+	curl -Lc "$cookieA" "$URL" > "$html"
+	hiddenData=$(hxnormalize -edxL "$html" | hxselect -s '\n' "input[type=hidden]" | egrep -v "name=\"type|name=\"hact" |  awk 'BEGIN{ORS="&"; FS="\""}{print $2"="$6}')
+
+	if [ -z "$(echo "$hiddenData" | grep fdkey)" ]; then
+		echo "Empty FDKEY!"
+		exit 1003
+	fi
+
+	# Check the login was in config
+	if [ -z "$login" ]; then
+		echo "Enter BUT login:"
+		read login
+
+		if [ -z "$login" ]; then
+			echo "Cannot use empty login!"
+			exit 1004
+		fi
+	else
+		echo "Using BUT login from config '$login'"
+	fi
+
+	# Prompt for pw
+	echo "Enter password:" 
+	read -s pw
+
+	if [ -z "$pw" ]; then
+		echo "Cannot use empty password!"
+		exit 1005
+	fi
+
+	data="${hiddenData}LDAPlogin=$login&LDAPpasswd=$pw"
+	URL="https://www.vutbr.cz/login/in"
+
+	justDoPOST
+}
+
+justDoPOST() {
+	tmpHtml="$html"
+	html="/dev/null"
+	getHtmlWithDataPOST
+	html="$tmpHtml"
+}
+
+assertURLnotEmpty() {
+
+	if [ -z "$URL" ]; then
+		echo "Cannot do request on unknown URL!"
+		exit 1001
+	fi
+}
+
+assertDataNotEmpty() {
+
+	if [ -z "$data" ]; then
+		echo "Cannot do POST with no data!"
+		exit 1002
+	fi
+}
+
+getHtmlWithDataPOST() {
+
+	assertDataNotEmpty
+	assertURLnotEmpty
+
+	switchCookies
+	curl -Lc "$cookieA" -b "$cookieB" -d "$data" "$URL" > "$html"
+}
+
+getHtml() {
+
+	assertURLnotEmpty
+
+	switchCookies
+	curl -Lc "$cookieA" -b "$cookieB" "$URL" > "$html"
+}
